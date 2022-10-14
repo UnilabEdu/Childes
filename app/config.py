93b00eab795ecf0c6036d317b452e90ee58b1b12
tcruzyy <nugo.svianadze123@gmail.com>
2022-10-14 20:41:57 +0400

    updates: project structure changes, dynamic routes

--- app/config.py
+++ app/config.py
@@ -5,14 +5,18 @@ import os
 SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-STATI_FOLDER = os.path.join(basedir, 'static')
-uploads_folder = os.path.join(basedir, 'uploads')
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+STATIC_FODLER = os.path.join(BASEDIR, 'static')
+UPLOADS_FOLDER = os.path.join(BASEDIR, 'uploads')
 # find css fold in app>main>front>css 
-main_statics_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'main', 'front')
-main_template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'main', 'html')
+main_statics_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'front')
+MAIN_TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+MAIN_TEMPLATE = os.path.join(MAIN_TEMPLATE_FOLDER, 'main')
+ADMIN_PANEL_TEMPLATES = os.path.join(MAIN_TEMPLATE_FOLDER, 'admin_panel')
 
-ADMIN_PANEL_FOLDER = os.path.join(basedir, 'admin_panel')
+print(main_statics_folder, MAIN_TEMPLATE_FOLDER)
+
+ADMIN_PANEL_FOLDER = os.path.join(BASEDIR, 'admin_panel')
 
 
 FLASK_ADMIN_SWATCH = 'cerulean'
@@ -25,6 +29,6 @@ MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 
 
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "data.sqlite")}'
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASEDIR, "data.sqlite")}'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
