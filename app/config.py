40628242acdfe9ef9a772205e72bca1bd2ba952e
sturua23 <you@example.com>
2022-10-23 19:27:03 +0400

    fixed reset password, add new html file pasw_html, and changed config file structure

--- app/config.py
+++ app/config.py
@@ -1,36 +1,44 @@
-
 # Flask application config that need to be set before creating the app
 import os
 
-SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-
-
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
 STATIC_FODLER = os.path.join(BASEDIR, 'static')
 UPLOADS_FOLDER = os.path.join(BASEDIR, 'uploads')
-# find css fold in app>main>front>css 
 main_statics_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'front')
 MAIN_TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 MAIN_TEMPLATE = os.path.join(MAIN_TEMPLATE_FOLDER, 'main')
 ADMIN_PANEL_TEMPLATES = os.path.join(MAIN_TEMPLATE_FOLDER, 'admin_panel')
 
 
+class Config:
+    """Base Config."""
+    ADMIN_PANEL_FOLDER = os.path.join(BASEDIR, 'admin_panel')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+
+    # find css fold in app>main>front>css
 
 
-ADMIN_PANEL_FOLDER = os.path.join(BASEDIR, 'admin_panel')
+class ProdConfig(Config):
+    """Production Config."""
+    FLASK_ADMIN_SWATCH = 'cerulean'
+    MAIL_SERVER = 'smtp.mailtrap.io'
+    MAIL_PORT = 2525
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = 'b751eb87d05854'
+    MAIL_PASSWORD = '5f89c3ea1bb1ef'
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASEDIR, "data.sqlite")}'
 
 
-FLASK_ADMIN_SWATCH = 'cerulean'
-MAIL_SERVER = 'smtp.mailtrap.io'
-MAIL_PORT = 2525
-MAIL_USE_TLS = True
-MAIL_USE_SSL = False
-MAIL_USERNAME = 'b751eb87d05854'
-MAIL_PASSWORD = '5f89c3ea1bb1ef'
-
-
-
-
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASEDIR, "data.sqlite")}'
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+class DevConfig(Config):
+    """Development Config."""
+    FLASK_ADMIN_SWATCH = 'cerulean'
+    MAIL_SERVER = 'smtp.mailtrap.io'
+    MAIL_PORT = 2525
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = 'b751eb87d05854'
+    MAIL_PASSWORD = '5f89c3ea1bb1ef'
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASEDIR, "data.sqlite")}'
